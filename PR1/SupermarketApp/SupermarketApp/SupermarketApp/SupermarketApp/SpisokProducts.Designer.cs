34f02acd98ebd524a724ef23885547af91b2220c
DanilSpraved <dspravedlivyj2@gmail.com>
2025-12-19 00:14:27 +0300

    Add files via upload

---
+++ PR1/SupermarketApp/SupermarketApp/SupermarketApp/SupermarketApp/SpisokProducts.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SupermarketApp
+{
+    partial class SpisokProducts
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbProducts = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lstSelectedProducts = new System.Windows.Forms.ListBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.textBox1txtTotal = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbProducts
+            // 
+            this.cmbProducts.FormattingEnabled = true;
+            this.cmbProducts.Location = new System.Drawing.Point(307, 103);
+            this.cmbProducts.Name = "cmbProducts";
+            this.cmbProducts.Size = new System.Drawing.Size(150, 21);
+            this.cmbProducts.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnAdd.Location = new System.Drawing.Point(318, 139);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(130, 31);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Добавить";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lstSelectedProducts
+            // 
+            this.lstSelectedProducts.FormattingEnabled = true;
+            this.lstSelectedProducts.Location = new System.Drawing.Point(328, 176);
+            this.lstSelectedProducts.Name = "lstSelectedProducts";
+            this.lstSelectedProducts.Size = new System.Drawing.Size(120, 95);
+            this.lstSelectedProducts.TabIndex = 2;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnClear.Location = new System.Drawing.Point(473, 248);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(82, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Очистить";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnCalculate.Location = new System.Drawing.Point(173, 248);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(121, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Посчитать итог";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // textBox1txtTotal
+            // 
+            this.textBox1txtTotal.Location = new System.Drawing.Point(434, 301);
+            this.textBox1txtTotal.Name = "textBox1txtTotal";
+            this.textBox1txtTotal.ReadOnly = true;
+            this.textBox1txtTotal.Size = new System.Drawing.Size(154, 20);
+            this.textBox1txtTotal.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(237, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Выберите продукты из списка ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(169, 296);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Итоговая сумма за покупку:";
+            // 
+            // SpisokProducts
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1txtTotal);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.lstSelectedProducts);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cmbProducts);
+            this.Name = "SpisokProducts";
+            this.Text = "Список продуктов";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbProducts;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.ListBox lstSelectedProducts;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.TextBox textBox1txtTotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PR1/SupermarketApp/SupermarketApp/SupermarketApp/SupermarketApp/SpisokProducts.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace SupermarketApp
+{
+    public partial class SpisokProducts : Form
+    {
+        // Строка подключения для SQL Server Express (самый частый случай)
+        private string connectionString = @"Server=.\SQLEXPRESS;Database=supermarket;Trusted_Connection=True;";
+
+        public SpisokProducts()
+        {
+            InitializeComponent();
+            LoadProducts();
+        }
+
+        private void LoadProducts()
+        {
+            cmbProducts.Items.Clear();
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                using (SqlCommand cmd = new SqlCommand("SELECT id, name, price FROM products", conn))
+                {
+                    conn.Open();
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            cmbProducts.Items.Add(new Product
+                            {
+                                Id = (int)reader["id"],
+                                Name = reader["name"].ToString().Trim(),
+                                Price = (decimal)reader["price"]
+                            });
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка: " + ex.Message, "База данных", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cmbProducts.SelectedItem is Product p)
+                lstSelectedProducts.Items.Add(p);
+            else
+                MessageBox.Show("Выберите продукт.");
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            lstSelectedProducts.Items.Clear();
+            textBox1txtTotal.Text = "";
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal sum = 0;
+            foreach (Product p in lstSelectedProducts.Items)
+                sum += p.Price;
+            textBox1txtTotal.Text = sum.ToString("C");
+        }
+    }
+}
